7d251cad6ae779dacb05e5dac583e0a5fa65eeba
Jose <chaparro.jjr@gmail.com>
2021-01-04 17:44:42 -0700

    items are saved to local storage and deleted

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ class Meal {
   }
 }
 
-
 class UI {
   addMealToList(meal) {
     const list = document.getElementById('meal-list');
@@ -54,138 +53,52 @@ class UI {
   }
 }
 
-// Event Listeners 
-document.getElementById('meal-form').addEventListener('submit', function (e) {
-  // Get Form Values 
-  const name = document.getElementById('name').value,
-    calories = document.getElementById('calories').value,
-    mealNum = document.getElementById('meal-num').value;
-  
-  // Instantiate the meal
-  const meal = new Meal(name, calories, mealNum);
-  // Instantiate UI 
-  const ui = new UI();
+// Local Storage 
+class Store {
+  static getMeals() {
+    let meals;
+    if (localStorage.getItem('meals') === null) {
+      meals = [];
+    } else {
+      meals = JSON.parse(localStorage.getItem('meals'));
+    }
 
-  // Validate 
-  if (name === '' || calories === "" || mealNum === "") {
-    // Show Alert 
-    ui.showAlert('Please fill in all fields', 'error');
-  } else {
-    // Add book to list 
-    ui.addMealToList(meal);
-
-    // Clear Fields 
-    ui.clearFields();
+    return meals;
   }
 
-  e.preventDefault();
-});
+  static displayMeals() {
+    const meals = Store.getMeals();
+    
+    meals.forEach(function (meal) {
+      const ui = new UI;
 
-// Event Listener for deleting 
-document.getElementById('meal-list').addEventListener('click', function (e) {
-  // Instantiate UI 
-  const ui = new UI();
+      // Add meal to UI 
+      ui.addMealToList(meal);
+    });
+  }
 
-  ui.deleteMeal(e.target);
+  static addMeal(meal) {
+    const meals = Store.getMeals();
 
-  // Show Message 
-  ui.showAlert('Book Removed', 'success');
+    meals.push(meal);
 
-  e.preventDefault();
-});
+    localStorage.setItem('meals', JSON.stringify(meals));
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// Meal constructor
-function Meal(name, calories, mealNum) {
-  this.name = name;
-  this.calories = calories;
-  this.mealNum = mealNum;
-}
-// UI Constructor 
-function UI() {
-  // Add meal to list
-  UI.prototype.addMealToList = function (meal) {
-    const list = document.getElementById('meal-list');
-    // Create tr element 
-    const row = document.createElement('tr');
-    // Insert Columns 
-    row.innerHTML = `
-      <td>${meal.name}</td>
-      <td>${meal.calories}</td>
-      <td>${meal.mealNum}</td>
-      <td><a class="delete">X</a></td>
-    `;
-
-    list.appendChild(row);
+  static removeMeal(mealNum) {
+    const meals = Store.getMeals();
+    
+    meals.forEach(function (meal, index) {
+      if (meal.mealNum === mealNum) {
+        meals.splice(index, 1);
+      }
+    });
+    localStorage.setItem('meals', JSON.stringify(meals));
   }
 }
 
-// Show Alert 
-UI.prototype.showAlert = function (message, className) {
-  // Create the div 
-  const div = document.createElement('div');
-  // Add class name 
-  div.className = `alert ${className}`;
-  // add text 
-  div.appendChild(document.createTextNode(message));
-  // Get Parent El 
-  const container = document.querySelector('.container');
-  const form = document.querySelector('#meal-form');
-  // Insert alert before the form 
-  container.insertBefore(div, form);
-  // Time out 
-  setTimeout(function () {
-    document.querySelector('.alert').remove();
-  }, 3000);
-}
-
-// Delete Meal 
-UI.prototype.deleteMeal = function (target) {
-  if (target.className === 'delete') {
-    target.parentElement.parentElement.remove();
-  }
-}
-
-// Clear Fields 
-UI.prototype.clearFields = function () {
-  document.getElementById('name').value = '';
-  document.getElementById('calories').value = '';
-  document.getElementById('meal-num').value = '';
-}
-
-
+// DOM LOAD Event 
+document.addEventListener('DOMContentLoaded', Store.displayMeals);
 
 // Event Listeners 
 document.getElementById('meal-form').addEventListener('submit', function (e) {
@@ -207,6 +120,9 @@ document.getElementById('meal-form').addEventListener('submit', function (e) {
     // Add book to list 
     ui.addMealToList(meal);
 
+    // Add to local storage 
+    Store.addMeal(meal);
+
     // Clear Fields 
     ui.clearFields();
   }
@@ -219,11 +135,14 @@ document.getElementById('meal-list').addEventListener('click', function (e) {
   // Instantiate UI 
   const ui = new UI();
 
+  // Delete meal
   ui.deleteMeal(e.target);
+
+  // Remove from LS 
+  Store.removeMeal(e.target.parentElement.previousElementSibling.textContent)
 
   // Show Message 
   ui.showAlert('Book Removed', 'success');
 
   e.preventDefault();
 });
-*/
