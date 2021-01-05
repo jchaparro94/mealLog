f9a94261e844204a97d224664b85ebc0a6f09f41
Jose <chaparro.jjr@gmail.com>
2021-01-04 17:28:39 -0700

    converted code to ES6 classes

--- app.js
+++ app.js
@@ -1,3 +1,132 @@
+class Meal {
+  constructor(name, calories, mealNum) {
+    this.name = name;
+    this.calories = calories;
+    this.mealNum = mealNum;
+  }
+}
+
+
+class UI {
+  addMealToList(meal) {
+    const list = document.getElementById('meal-list');
+    // Create tr element 
+    const row = document.createElement('tr');
+    // Insert Columns 
+    row.innerHTML = `
+      <td>${meal.name}</td>
+      <td>${meal.calories}</td>
+      <td>${meal.mealNum}</td>
+      <td><a class="delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+
+  showAlert(message, className) {
+    // Create the div 
+    const div = document.createElement('div');
+    // Add class name 
+    div.className = `alert ${className}`;
+    // add text 
+    div.appendChild(document.createTextNode(message));
+    // Get Parent El 
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#meal-form');
+    // Insert alert before the form 
+    container.insertBefore(div, form);
+    // Time out 
+    setTimeout(function () {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+
+  deleteMeal(target) {
+    if (target.className === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
+  }
+
+  clearFields() {
+    document.getElementById('name').value = '';
+    document.getElementById('calories').value = '';
+    document.getElementById('meal-num').value = '';
+  }
+}
+
+// Event Listeners 
+document.getElementById('meal-form').addEventListener('submit', function (e) {
+  // Get Form Values 
+  const name = document.getElementById('name').value,
+    calories = document.getElementById('calories').value,
+    mealNum = document.getElementById('meal-num').value;
+  
+  // Instantiate the meal
+  const meal = new Meal(name, calories, mealNum);
+  // Instantiate UI 
+  const ui = new UI();
+
+  // Validate 
+  if (name === '' || calories === "" || mealNum === "") {
+    // Show Alert 
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list 
+    ui.addMealToList(meal);
+
+    // Clear Fields 
+    ui.clearFields();
+  }
+
+  e.preventDefault();
+});
+
+// Event Listener for deleting 
+document.getElementById('meal-list').addEventListener('click', function (e) {
+  // Instantiate UI 
+  const ui = new UI();
+
+  ui.deleteMeal(e.target);
+
+  // Show Message 
+  ui.showAlert('Book Removed', 'success');
+
+  e.preventDefault();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // Meal constructor
 function Meal(name, calories, mealNum) {
   this.name = name;
@@ -96,4 +225,5 @@ document.getElementById('meal-list').addEventListener('click', function (e) {
   ui.showAlert('Book Removed', 'success');
 
   e.preventDefault();
-})
+});
+*/
